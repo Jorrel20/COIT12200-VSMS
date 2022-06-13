e691562b5226f0223d529a9f0e8e92256b87a707
AcmeCat <allenruaidhri@gmail.com>
2022-06-13 20:14:24 +0800

    service domain class built

--- VSMS_2/src/domain/Service.java
+++ VSMS_2/src/domain/Service.java
@@ -4,6 +4,73 @@
  */
 package domain;
 
+import java.sql.Date;
+
 public class Service {
+    
+    //instance variables
+    private int serviceID;
+    private String details;
+    private Date date;
+    private double price;
+    private String regoNumber;
+    
+    //constructor
+    public Service(int serviceID, String details, Date date, double price, String regoNumber) {
+        this.serviceID = serviceID;
+        this.details = details;
+        this.date = date;
+        this.price = price;
+        this.regoNumber = regoNumber;
+    }
+    
+    //accessors/modifiers
+
+    public int getServiceID() {
+        return serviceID;
+    }
+
+    public void setServiceID(int serviceID) {
+        this.serviceID = serviceID;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getRegoNumber() {
+        return regoNumber;
+    }
+
+    public void setRegoNumber(String regoNumber) {
+        this.regoNumber = regoNumber;
+    }
+    
+    //tostring
+    @Override
+    public String toString() {
+        return "Service{" + "serviceID=" + serviceID + ", details=" + details + ", date=" + date + ", price=" + price + ", regoNumber=" + regoNumber + '}';
+    }
+    
     
 }
